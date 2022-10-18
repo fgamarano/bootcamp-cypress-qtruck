bd537e74be5f1a8afaef00e4007d8f0d1819e931
fgamarano <fehgamarano@gmail.com>
2022-10-18 06:54:08 -0300

    alure

--- cypress/e2e/review.cy.js
+++ cypress/e2e/review.cy.js
@@ -1,39 +1,24 @@
 import mapPage from '../support/pages/map'
 import foodtruckPage from '../support/pages/Foodtruck'
+import createReview from '../fixtures/review'
 
 describe('Avaliações', () => {
-
-    const user = {
-        name: 'Ana Banana',
-        instagram: '@aninha',
-        password: 'pwd123'
-    }
-
-    const foodtruck = {
-        latitude: '-19.97131190315524',
-        longitude: '-43.96547198295593',
-        name: 'Sandubão do Leo',
-        details: 'O melhor podrão da região.',
-        opening_hours: 'das 20h às 04h',
-        open_on_weekends: true
-    }
-
-    const review = {
-        comment: "Sanduiche muito gorduroso porém a coca tava gelada.",
-        stars: "3"
-    }
+   
+    //console.log(JSON.stringify(var));
+    
+    const data = createReview
 
     it('Deve avaliar um foodtruck', () => {
 
-        cy.apiCreateUser(user)
-        cy.apiLogin(user)
-        cy.apiCreateFoodTruck(foodtruck)
+        cy.apiCreateUser(data.user)
+        cy.apiLogin(data.user)
+        cy.apiCreateFoodTruck(data.foodtruck)
 
-        cy.uiLogin(user)
+        cy.uiLogin(data.user)
 
-        mapPage.goToFoodtruck(foodtruck.name)
-        foodtruckPage.addReview(review)
-        foodtruckPage.reviewShouldBe(user, review)
+        mapPage.goToFoodtruck(data.foodtruck.name)
+        foodtruckPage.addReview(data.review)
+        foodtruckPage.reviewShouldBe(data.user, data.review)
 
     })
 })
